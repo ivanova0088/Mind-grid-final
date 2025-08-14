982eba1d3420a2e39171c04c9f442595c59c76fb
ivanova0088 <ivan.hbo88@gmail.com>
2025-08-14 23:53:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-// ===== app.js =====
-import { boot, showScreen, renderUsers, saveUser, enterAs, delUser, cycleTheme,
-         addDevCoins, addDevGems } from './js/core.js?v=modA7';
+// ===== app.js (module entry) =====
+import {
+  boot, showScreen, renderUsers, saveUser, enterAs, delUser,
+  cycleTheme, addDevCoins, addDevGems
+} from './js/core.js?v=modA8';
 
-import { startOdyssey, odPrev, odNext, odCheck, odFinishStage, odKey } from './js/odyssey.js?v=modA7';
+import {
+  startOdyssey, odPrev, odNext, odCheck, odFinishStage, odKey
+} from './js/odyssey.js?v=modA8';
 
-import { openCrossword, cwToggleSheet, cwSetDir, cwPrev, cwNext, cwCheckWord, cwCheckAll,
-         cwHintLetter, cwFlipDir, cwMarkLater, cwSearch, cwFilter } from './js/crossword.js?v=modA7';
+import {
+  openCrossword, cwToggleSheet, cwSetDir, cwPrev, cwNext, cwCheckWord, cwCheckAll,
+  cwHintLetter, cwFlipDir, cwMarkLater, cwSearch, cwFilter
+} from './js/crossword.js?v=modA8';
 
-window.showScreen=showScreen; window.renderUsers=renderUsers; window.saveUser=saveUser;
-window.enterAs=enterAs; window.delUser=delUser; window.cycleTheme=cycleTheme;
-window.addDevCoins=addDevCoins; window.addDevGems=addDevGems;
+window.showScreen   = showScreen;
+window.renderUsers  = renderUsers;
+window.saveUser     = saveUser;
+window.enterAs      = enterAs;
+window.delUser      = delUser;
+window.cycleTheme   = cycleTheme;
+window.addDevCoins  = addDevCoins;
+window.addDevGems   = addDevGems;
 
-window.startOdyssey=startOdyssey; window.odPrev=odPrev; window.odNext=odNext;
-window.odCheck=odCheck; window.odFinishStage=odFinishStage; window.odKey=odKey;
+window.startOdyssey   = startOdyssey;
+window.odPrev         = odPrev;
+window.odNext         = odNext;
+window.odCheck        = odCheck;
+window.odFinishStage  = odFinishStage;
+window.odKey          = odKey;
 
-window.openCrossword=openCrossword; window.cwToggleSheet=cwToggleSheet; window.cwSetDir=cwSetDir;
-window.cwPrev=cwPrev; window.cwNext=cwNext; window.cwCheckWord=cwCheckWord; window.cwCheckAll=cwCheckAll;
-window.cwHintLetter=cwHintLetter; window.cwFlipDir=cwFlipDir; window.cwMarkLater=cwMarkLater;
-window.cwSearch=cwSearch; window.cwFilter=cwFilter;
+window.openCrossword  = openCrossword;
+window.cwToggleSheet  = cwToggleSheet;
+window.cwSetDir       = cwSetDir;
+window.cwPrev         = cwPrev;
+window.cwNext         = cwNext;
+window.cwCheckWord    = cwCheckWord;
+window.cwCheckAll     = cwCheckAll;
+window.cwHintLetter   = cwHintLetter;
+window.cwFlipDir      = cwFlipDir;
+window.cwMarkLater    = cwMarkLater;
+window.cwSearch       = cwSearch;
+window.cwFilter       = cwFilter;
 
 document.addEventListener('DOMContentLoaded', boot);
